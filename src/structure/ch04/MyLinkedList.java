ce632b2241ad84b374b6dc807741090d95d0b425
dbstjlee <dbstjlee@naver.com>
2024-05-23 18:17:49 +0900

    2024-05-23

--- src/structure/ch04/MyLinkedList.java
+++ src/structure/ch04/MyLinkedList.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class MyLinkedList {
 
 	private Node head; // 포함관계 // 요소의 맨 처음을 가리킴.
-	private int count; // 링크드 리스트에 몇개의 요소가 연결되어 있는 개수
+	private int count; // Linked List에 몇개의 요소가 연결되어 있는 개수
 
 	// MyLinkedList를 생성하면 맨 처음 생성 시 노드는 없는 상태이기 때문에 null임.
 	public MyLinkedList() {
@@ -22,113 +22,163 @@ public class MyLinkedList {
 	// 새로운 Node 클래스를 1개 추가하는 메서드 생성
 	public Node addElement(String data) {
 		Node createNode = new Node(data); // 노트 객체 생성 시 데이터가 담길 수 있도록 생성
-
+		// if문: 인덱스 0: addElement("야스오") => 야스오가 createNode에 담김
+		// else문: 인덱스 1: addElement("티모")
 		if (head == null) {
 			// 맨 처음 요소를 저장하는 동작
-			// 항상 head 부터 찾아야 Linked list로 간다?고 함.
-			head = createNode; // head 에 넣어주면 저장이 됨.
-			// head에 들어가있음
+			// 항상 head부터 찾아야한다.
+			head = createNode; // head에 넣어주면 저장이 됨.
+			// head가 null이어서 createNode에 담겨있던 야스오가 head에 들어감.
 			// 맨 처음 head를 지정해줌.
 		} else {
-			// 항상 요소 찾기는 head 부터 시작
+			// head에는 야스오가 담겨있고, 티모가 추가되는 상황
+			// 항상 요소 찾기는 head부터 시작
 			Node preNode = head;// 내가 찾을 데이터 타입: Node
-			// [야스오][](next) =>null 이라 while 문 안 돌고 핵심 코드에 들어감
+			// head에 담겨있던 야스오를 preNode에 담아둠.
+			// => preNode = 야스오
+			// preNode.next : 티모 (null)이 아니다.
+			
 			// preNode.next //<--- Node를 가리킴 <-- 주소값이 없으면 null
-			while (preNode.next != null) { // 다음 요소가 있는 것을 의미함
-				preNode = preNode.next;
+			//preNode[야스오][null]
+			// preNode[야스오][티모]
+			while (preNode.next != null) { // 다음 요소가 있을때
+				preNode = preNode.next; // 야스오가 담겨있던 preNode에 티모의 주소값이 담김
+				//[야스오][티모.주소값] --> [티모][null]
+				// 티모 --> [티모][null]
 			}
+			// [야스오][] => null 이라 while문 안 돌고 핵심 코드에 들어감
 			// 핵심 코드
-			// 티모에 주소값이 담김
-			// [티모][next] --> [next] = [소라카][] // next 요소에 소라카가 담겨 있음
-			preNode.next = createNode;
+			// preNode.next가 null 이면(비어있음)
+			// [야스오][] --> [] = 티모의 주소값이 담김
+			//preNode[야스오][티모]
+			// [티모][소라카]
+			preNode.next = createNode; // createNode에 담겨있는 티모의 주소값이 preNode.next에 담김
+			// [야스오][티모.주소값] --> [티모][null] (뒤에 요소가 없다면 null 임)
+			
 		}
-		count++; // 요소가 1개 증가함
-		return createNode; 
+		count++; // 요소가 1개 증가함(티모가 추가됨)
+		return createNode; // 티모가 반환됨.
+		// 출력: [야스오][티모.주소값] --> [티모][null]
 	}
 
+	// 노드를 1개 제거하는 메서드
+	// [야스오][티모.주소값] --> [티모][소라카.주소값] --> [소라카][애쉬.주소값] -->[애쉬][null]
+	// 2 
 	public Node removeElement(int index) {
 		// 방어적 코드
-		// count는 인덱스 개념? 아무튼 index와 count 의 개념이 다름
-		if (index >= count) { // 사용자가 요청한 인덱스 값이 존재하는 인덱스 개수보다 크거나 같을때
+		// index: 사용자가 요청한 값
+		// count는 인덱스 개념 (index와 count의 개념이 다름)
+		if (index >= count) { // 사용자가 요청한 값(인덱스)이, 존재하는 인덱스 개수(count)보다 크거나 같을때
 			System.out.println("삭제할 위치 오류. 현재 리스트 개수는 " + count + "입니다.");
-			return null; // Node 데이터 타입이라서 null을 반환함
+			return null; // 리턴 타입인 Node가 데이터 타입이라서 null을 반환함
 		}
 		// 실행 시점 생각하기
 		// 맨 앞 요소를 삭제 요청 시
 		// 항상 요소를 찾을 때 시작은 head 부터 시작이다.
-		// index: 사용자가 요청한 값
 		Node tempNode = head;
-		if (index == 0) { // 인덱스 0번(야스오)라면 
+		// head에 담긴 야스오를 tempNode에 담는다.
+		if (index == 0) { // 인덱스 0번(야스오)라면
 			// 코드 시작 전 모습
-			// [야스오][티모.주소값(티모의 주소값)] --> [티모][null] (뒤에 요소가 없다면 null 임)
-			head = tempNode.next; // 런타임 시점 // 다음 주소값을 head (야스오)에 담음.
+			// [야스오][티모.주소값] --> [티모][소라카.주소값] --> [소라카][애쉬.주소값] -->[애쉬][null]
+			head = tempNode.next; // 런타임 시점 // 다음 주소값(티모)을 비어있는 head에 담음.
 			// 코드 수행 후 모습
-			// [티모][null] // 뒤에 있는 티모를 head로 옮김.
+			// [티모][소라카.주소값] --> [소라카][애쉬.주소값] -->[애쉬][null](야스오 삭제됨)
 		} else {
 			// 코드 시작 전 모습 => index = 2 이라고 가정 ---> 인덱스 값은 n-1 이라서 1임.
 			// 0 1 2
-			// [야스오][티모.주소값] --> [티모][소라카.주소값] --> [소라카][null]
+			// [야스오][티모.주소값] --> [티모][소라카.주소값] --> [소라카][애쉬.주소값] -->[애쉬][null] (소라카 삭제가 목적)
 			// 링크를 제거하고 0의 주소값을 2라고 바꿔치기하기
-			// 이 부분 그림 그려서 이해하기
 			Node preNode = null;// 지역변수 선언 // preNode(head(0번째))에 null 잠시 담아두기. 그래야 찾아갈 수 있음.
 			for (int i = 0; i < index; i++) {
-				// 0, 1
-				preNode = tempNode; // 0번째에 항상 head(=tempNode) 가 들어감. //head(=tempNode)를  prenode(null)에 담고(null <-- head 담기)
-				tempNode = tempNode.next; // 다음(1번째)를 tempnode(head)에 담기(비어있는 head <-- 1번째 담기)
+				// index = 2
+				
+				// 두번째 반복 - 티모 
+				// pre - 티모
+				// temp - 소라카
+				
+				preNode = tempNode; // 0번째에 항상 head가 들어감.
+				// 야스오가 담겨있는 tempNode(head)를 preNode(null)에 담기
+				tempNode = tempNode.next; // 다음(티모) 요소를 비어있는 tempNode에 담기
+				// preNode = 야스오 tempNode = 티모
+				
+				// 1
+				//preNode = tempNode; // 야스오가 담겨있는 preNode에 티모가 담겨있는 tempNode를 담음
+				//tempNode = tempNode.next; // 다음(소라카) 요소를 비어있는 tempNode에 담기
+				// preNode = 티모 tempNode = 소라카
 			}
-			preNode.next = tempNode.next;// 반복문 끝나면 (null 다음꺼 <-- 1번째 담기)
+			
+			// pre - 티모
+			// temp - [소라카][애쉬]
+			
+			// 반복문 끝나면
+			// [티모][소라카]  = tempNode.next
+			//[티모][애쉬]     
+			preNode.next = tempNode.next;// 애쉬의 주소값을 소라카의 주소값에 담기
+			// [티모][소라카.주소값] --> [소라카][애쉬.주소값]-->[애쉬][null]
+			// [소라카.주소값] <-- [애쉬.주소값]
+			// [티모][애쉬.주소값]-->[애쉬][null] (야스오는?)
+			// 출력: [야스오][티모.주소값]-->[티모][애쉬.주소값]-->[애쉬][null]
+			
 
 		} // end of if
 		count--; // 요소를 하나 삭제함.
-		//System.out.println(position + "번째 요소를 삭제했습니다. ");
+		// System.out.println(position + "번째 요소를 삭제했습니다.");
 
 		// if 문 없으면 아무것도 없는 것과 마찬가지라서 return 값 넣기
-		return tempNode; // 찾았던 tempNode를 리턴시킴
+		// [티모][소라카] 
+		return tempNode; // 찾았던 tempNode를 리턴시킴 => 여기서 야스오를 리턴??
 	}
-	
+
 	// 전체 출력하는 기능 만들기
 	public void printAll() {
-		if(count == 0) {
-			System.out.println("출력할 내용이 없습니다. ");
+		if (count == 0) {
+			System.out.println("출력할 내용이 없습니다.");
 			return;
 		}
 		Node temp = head; // 항상 head 부터 시작함.
-		while(temp != null) {
-			System.out.print(temp.getData());
-			temp = temp.next; // 다음 요소를 담음
-			if(temp != null) {
+		while (temp != null) {
+			System.out.print(temp.getData()); //head가 null이 아니면 data가 출력
+			temp = temp.next; // 다음 요소를 앞의 요소로 반복해서 당김.
+			if (temp != null) {
 				System.out.print("-->");
 			}
 		}
 		System.out.println();// 줄바꿈
 	}
-	
+
 	// 지정한 위치의 노드 하나만 출력하기
-	public Node getNodeByIndex(int index) { //index 를 받을 수 있게 설계
-		if(index >= count) {
-			System.out.println("검색 위치 요류 - 잘못된 요청");
+	public Node getNodeByIndex(int index) { // index 를 받을 수 있게 설계
+		if (index >= count) {
+			System.out.println("검색 위치 오류 - 잘못된 요청");
 		}
-		Node tempNode = head; //-> 멤버 변수 선언해서 여기서 담고 활용하기
-		if(index == 0) {
+		Node tempNode = head; // -> 멤버 변수 선언해서 여기서 담고 활용하기
+		// head(야스오)를 tempNode에 담음
+		if (index == 0) {
 			return head; // 맨 처음이라는 것을 인지하기 위해 head 사용(tempNode 들어가도 무방함)
 		}
-		//  Node 에는 String data와 Node에는 다음 주소값이 담김
+		// Node에는 String data와 다음 주소값이 담김
 		// 다음 요소를 찾기 위해서는 .next (= 다음 요소)를 원래 요소에 담아줌.
-		// [야스오][티모.주소값] ->[티모][] ->[소라카][]
-		for(int i = 0; i < index; i++) {
-			tempNode = tempNode.next; // 다음 요소는 무조건 next에 담겨 있다. 
-			// 다음 요소를 tempNode 에 계속 담아 주기
+		// [야스오][티모.주소값] ->[티모][애쉬.주소값] ->[애쉬][null]
+		for (int i = 0; i < index; i++) {
+			// index가 2라면
+			tempNode = tempNode.next; // 다음 요소는 무조건 next에 담겨 있다.
+			// 다음 요소를 tempNode에 계속 담아 주기
+			// tempNode: 야스오 tempNode.next: 티모
+			// => 티모가 tempNode에 담김
+			
+			// 1
+			//tempNode = tempNode.next; 
+			// tempNode: 티모 tempNode.next: 애쉬
+			// => 애쉬가 tempNode에 담김
 		}
-		return tempNode; // 지역 변수를 return시킴
+		return tempNode; // 애쉬를 반환시킴
 	}
-	
+
 	// 전체 삭제 기능
 	public void removeAll() {
 		head = null;
 		count = 0; // 상태값 관리하기
 	}
-	
-	
 
 	// 테스트 코드
 	public static void main(String[] args) {
@@ -138,21 +188,20 @@ public class MyLinkedList {
 		linkedList.addElement("티모");
 		linkedList.addElement("소라카");
 		linkedList.addElement("애쉬");
-		linkedList.addElement("가렌");
+//		linkedList.addElement("가렌");
 
-		
 		linkedList.printAll();
-		linkedList.removeElement(2);
-		linkedList.printAll();
-		// head
-		// [야스오][]---->[티모][]---->[소라카][]
-		// .연산자 사용x 객체를 사용해야 함.
-		//System.out.println(linkedList.getCount()); // 저장된 노드의 개수 : 3개
 		
-		System.out.println(linkedList.getNodeByIndex(1).getData());// 주소.getData()
-		linkedList.removeAll(); // heap 영역의 객체는 살아 있지만 link는 삭제해서 찾을 수 없음 
-		//-> 가비지가 알아서 삭제
+		linkedList.removeElement(2); // 인덱스 2번 => 세 번째인 소라카가 삭제됨.
+		// [야스오][]---->[티모][]---->[애쉬][] 
+		//linkedList.addElement("소라카");// 삭제한 소라카를 다시 추가하면 맨 뒤에 추가됨.
+		linkedList.printAll();
+		
+		// .연산자 사용x. 객체를 사용해야 함.
+		System.out.println(linkedList.getCount()); // 저장된 노드의 개수 : 4개(5개 중 1개 삭제)
+		System.out.println(linkedList.getNodeByIndex(2).getData());// 주소.getData() // 인덱스 2번 :애쉬
+		linkedList.removeAll(); // heap 영역의 객체는 살아있지만 link는 삭제해서 찾을 수 없음
+		// -> 가비지가 알아서 삭제
 		linkedList.printAll();
 	}// end of main
-
 }// end of class
--- src/structure/ch04/Node.java
+++ src/structure/ch04/Node.java
@@ -2,34 +2,30 @@ package structure.ch04;
 
 public class Node {
 
-	private String data; //String 이라 "A"라는 데이터가 담길 수 있음
+	private String data; // String 이라 "A"라는 데이터가 담길 수 있음
+	public Node next;
 	// 자기 자신의 데이터 타입을 가짐(자기 참조 관계)(셀프 참조)
-	// 정의 : 자기 참조는 객체가 자신과 같은 타입의 다른 객체를
-	// 가리키는 경우를 말한다.
+	// 정의 : 자기 참조는 객체가 자신과 같은 타입의 다른 객체를 가리키는 경우를 말한다.
 	// 용도 : 연결 리스트, 트리 구조에 많이 활용
-	public Node next; 
-	
+
 	// 기본 생성자
 	public Node() {
 		data = null;
-		next = null; //Node를 가리킴
+		next = null; // Node를 가리킴
 	}
+
 	public Node(String data) {
 		this.data = data;
-		next = null; //처음 생성되는 거라 null
+		next = null; // 처음 생성되는 거라 null
 	}
+
 	public Node(String data, Node link) {
 		this.data = data;
 		next = link; // 같은 데이터 타입이라 link를 받을 수 있음
 	}
-	
-	//getter
+
+	// getter
 	public String getData() {
 		return data;
 	}
-	
-
-	
-	
-	
 }
